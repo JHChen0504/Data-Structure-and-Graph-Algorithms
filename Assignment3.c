ed399756741bf6af6c3eae2f5cc17be4dcbafb03
JHChen0504 <119959373+JHChen0504@users.noreply.github.com>
2024-10-09 04:54:24 +0800

    Add files via upload

--- Assignment3.c
+++ Assignment3.c
@@ -2,7 +2,7 @@
 // path from the start cell s to the destination cell t in a maze of dimension m × n.
 //
 // author: C. H. Chen
-// date: 2024/10/7
+// date: 2024/10/9
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,73 +13,118 @@
 #include "maze.h"
 
 #define INFTY 2147483647
+#define dir 4
 
 typedef struct {
     short int v, h;
 }direction;
 
-direction move[4] = {{-1,0},{0,1},{1,0},{0,-1}};
+direction move[dir] = {{1,0},{-1,0},{0,1},{0,-1}};
 
-int* mazeBFS(const maze_t* maze){
+/*  opt = 0 => end when reach terminal. 
+*   opt = 1 => search whole maze. */
+int mazeBFS(maze_t* maze, int distance[], const int opt)
+{
     assert(maze != NULL);
-    int height = maze->height;
-    int width = maze->width;
-    int size = width * height;
-    int distance[size];
-    for (int row = 0; row < height; row++)
+    for (int row = 0; row < maze->height; row++)
     {
-        for (int col = 0; col < width; col++)
+        for (int col = 0; col < maze->width; col++)
         {
             position_t temp = {row, col};
-            switch (maze->cells[offset(&maze, temp)])
-            {
-                case START:
-                    distance[offset(&maze, temp)] = 0;
-                    break;
-                case BLOCKED:
-                    distance[offset(&maze, temp)] = -1;
-                    break;
-                default:
-                    distance[offset(&maze, temp)] = INFTY;
-                    break;
-            }
+            distance[offset(maze, temp)] = INFTY;
         }
     }
-    list_t *l;
-    list_add(maze->start,&l);
-    queue_t *q;
-    queue_initialize(&q);
-    enqueue(&q, &l);
+    distance[offset(maze, maze->end)] = 0;
+    list_t * point = (list_t*)malloc(sizeof(list_t));
+    list_t * adjacent_point = (list_t*)malloc(maze->height * maze->width * sizeof(list_t));
+    position_t adjacent;
+    point->position = maze->end;
+    queue_t q = {NULL, NULL};
+    enqueue(&q, point);
+    int flag = 0;
     while (!queue_empty(&q))
     {
-        for (int i = 0; i < 4; i++)
+        point->position = dequeue(&q)->position;
+        for (int i = 0; i < dir; i++)
         {
-            position_t temp;
-            temp.col = q->head->solution->position.col+move[i].v;
-            temp.row = q->head->solution->position.row+move[i].h;
-            switch (maze->cells[offset(&maze, temp)])
+            adjacent.col = point->position.col + move[i].h;
+            adjacent.row = point->position.row + move[i].v;
+            if (adjacent.col < 0 || adjacent.row < 0 || adjacent.col > maze->width || adjacent.row > maze->height){
+                continue;
+            }
+            adjacent_point->position = adjacent;
+            switch (maze->cells[offset(maze, adjacent)])
             {
             case OPEN:
-                enqueue(&q, &l);
-                maze->cells[offset(&maze, temp)] = VISITED;
-                distance[offset(&maze, temp)] = distance[offset(&maze, q->head->solution->position)] + 1;
-                break;
-            case END:
-                distance[offset(&maze, temp)] += 1;
+                enqueue(&q, adjacent_point);
+                adjacent_point++;
+                maze->cells[offset(maze, adjacent)] = VISITED;
+                distance[offset(maze, adjacent)] = distance[offset(maze, point->position)] + 1;
+                break; 
+            case START:
+                if (!opt && !flag)
+                {
+                    flag = 1;
+                    distance[offset(maze, adjacent)] = distance[offset(maze, point->position)] + 1;
+                    queue_initialize(&q);
+                }
+                else if (!flag)
+                {
+                    flag = 1;
+                    enqueue(&q, adjacent_point);
+                    adjacent_point++;
+                    distance[offset(maze, adjacent)] = distance[offset(maze, point->position)] + 1;
+                }
                 break;
             default:
                 break;
             }
         }
     }
+    free(adjacent_point);
+    free(point);
+}// mazeBFS
+
+list_t *shortest_path(maze_t* maze, int distance[]){
+    list_t *path = (list_t*)malloc(sizeof(list_t));
+    position_t adjacent;
+    path->next = NULL;
+    path->position = maze->start;
+    int d = distance[offset(maze, maze->start)];
+    if (d == INFTY)
+    {
+        printf("No path from start to end.\n");
+        return NULL;
+    }
+    while (d > 0)
+    {
+        for (int i = 0; i < dir; i++)
+        {
+            adjacent.col = path->position.col + move[i].v;
+            adjacent.row = path->position.row + move[i].h;
+            if(distance[offset(maze, adjacent)] == d - 1){
+                d--;
+                if(d!=0)maze->cells[offset(maze, adjacent)] = PATH;
+                path = list_add(adjacent,path);
+                i = dir;
+            }
+        }
+    }
+    return path;
 }
 
 
 int main(void){
     FILE * fp;
-    fp = fopen ("maze.txt", "r");
+    fp = fopen ("maze3.txt", "r");
     maze_t *maze = readMaze(fp);
     fclose(fp);
-
+    int distance[maze->height * maze->width];
+    mazeBFS(maze, distance, 0);
+    list_t *path = shortest_path(maze, distance);
+    printMaze(maze);
+    list_print_reverse(path);
+    list_free(path);
+    freeMaze(maze);
     return 0;
 }
